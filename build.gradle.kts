455b9d00ac438ae29988fc5cee9cf993a7cb84c2
karoly.belokostolsky <karoly.belokostolsky@gmail.com>
2024-05-24 22:14:12 +0200

    add liquibase dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-    implementation("org.flywaydb:flyway-core:10.5.0")
+    implementation("org.liquibase:liquibase-core:4.28.0")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
