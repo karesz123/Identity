1094c04cfbaa7e529a81d0de03aedadd58ff1c64
karoly.belokostolsky <karoly.belokostolsky@gmail.com>
2024-05-29 23:17:02 +0200

    add missing repository annotation

--- src/main/java/org/identity/repository/PhoneRepository.java
+++ src/main/java/org/identity/repository/PhoneRepository.java
@@ -2,6 +2,8 @@ package org.identity.repository;
 
 import org.identity.entity.PhoneEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PhoneRepository extends JpaRepository<PhoneEntity, String> {
 }
