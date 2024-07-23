ec0fe5e87e672dd6ef206ce607dcc98292a0fded
Karoly Belokostolsky <frkaresz123@gmail.com>
2024-07-23 21:59:42 +0200

    add method to personRepository

--- src/main/java/org/identity/repository/PersonRepository.java
+++ src/main/java/org/identity/repository/PersonRepository.java
@@ -4,6 +4,11 @@ import org.identity.entity.PersonEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PersonRepository extends JpaRepository<PersonEntity, String> {
+
+    @Override
+    List<PersonEntity> findAll();
 }
