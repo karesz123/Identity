e3d57d2ed0a9e3586f446695de46ebeb32adb8b3
Karoly Belokostolsky <frkaresz123@gmail.com>
2024-08-06 11:51:06 +0200

    fix jwt configuration

--- src/main/java/org/identity/configurations/JwtConfig.java
+++ src/main/java/org/identity/configurations/JwtConfig.java
@@ -3,13 +3,15 @@ package org.identity.configurations;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
 @Data
+@Configuration
 public class JwtConfig {
 
-    @Value("identity.jwtExpirationInMs")
+    @Value("${identity.jwtExpirationInMs}")
     private int jwtExpirationMs;
 
-    @Value("identity.jwtSecretKey")
+    @Value("${identity.jwtSecretKey}")
     private String jwtSecretKey;
 }
