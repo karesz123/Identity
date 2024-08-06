abc8099077bd6f819050e1512ccfa3f50f62096a
Karoly Belokostolsky <frkaresz123@gmail.com>
2024-08-06 11:51:40 +0200

    update security config

--- src/main/java/org/identity/configurations/SecurityConfig.java
+++ src/main/java/org/identity/configurations/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.identity.configurations;
 
+import lombok.RequiredArgsConstructor;
+import org.identity.filter.AuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,23 +11,28 @@ import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final String ENCODING_ID = "argon2" ;
 
+    private final AuthenticationFilter authenticationFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.authorizeHttpRequests(request ->
                 request
                         .anyRequest()
-                        .permitAll())
-                .csrf(AbstractHttpConfigurer::disable);
+                        .authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 
