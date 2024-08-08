71c83ca14433d27d87f9ad4203289142d3b6bf05
Karoly Belokostolsky <frkaresz123@gmail.com>
2024-08-08 10:47:29 +0200

    add spring boto validation starter

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
     implementation("org.bouncycastle:bcprov-jdk18on:1.78.1")
     implementation("org.modelmapper:modelmapper:3.2.0")
 
